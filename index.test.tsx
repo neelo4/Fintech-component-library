0571bc8148df18b3427fea586899a1c0bbc6a530
Haya <haya@Hayas-Laptop.local>
2026-01-21 14:03:18 +0000

    add quality gates

--- index.test.tsx
+++ index.test.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import { Button } from "./src/components/Button/Button";
+import { axe } from "jest-axe";
 
 describe("Button", () => {
   test("renders with children", () => {
@@ -33,5 +34,10 @@ describe("Button", () => {
     const button = screen.getByText("Click me");
     expect(button).toBeDisabled();
     expect(button).toHaveClass("opacity-50", "cursor-not-allowed");
+  });
+  test("Button is accessible", async () => {
+    const { container } = render(<Button>Click</Button>);
+    const results = await axe(container);
+    expect(results).toHaveNoViolations();
   });
 });
